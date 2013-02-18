0ba1a66d0dd3d0d2c90980ab9f8f0378736cb54e
OpenShift guest <andrew.james.gaspar@gmail.com>
2013-02-18 03:38:08 -0500

    Forgot AGMath.js

---
+++ src/AGMath.js
@@ -0,0 +1,16 @@
+/// <reference path="lib/require.js" />
+define("AGMath", ["Matrix/Matrix", "Matrix/Vector", "Numbers/JSNumber", "Numbers/ComplexNumber", "Numbers/FractionalNumber"], function (Matrix, Vector, JSNumber, ComplexNumber, FractionalNumber) {
+
+    return {
+        Matrix: {
+            Matrix: Matrix,
+            Vector: Vector
+        },
+        Numbers: {
+            JSNumber: JSNumber,
+            ComplexNumber: ComplexNumber,
+            FractionalNumber: FractionalNumber
+        }
+    };
+
+});
